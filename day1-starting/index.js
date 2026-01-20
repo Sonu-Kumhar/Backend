eb0e8913eef01a4456bf7402e9fb6faf7f2d5eed
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-20 20:29:52 +0530

    added figlet package for characters on cat

--- day1-starting/index.js
+++ day1-starting/index.js
@@ -1,7 +1,15 @@
 const lolcatjs = require('lolcatjs')
 const catMe = require('cat-me')
+const figlet = require('figlet')
 
 lolcatjs.options.seed = Math.floor(Math.random() * 1000);
 lolcatjs.options.colors = true;
 
 lolcatjs.fromString(catMe());
+
+async function doStuff() {
+  const text = await figlet.text("Meow");
+  console.log(text);
+}
+
+doStuff();
