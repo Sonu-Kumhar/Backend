1c0a11f4f49d4eebe5eb949dd06d873a43c3b2a0
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-23 12:28:37 +0530

    using post for creating the notes on server and retrieving them using get method

--- day3-restApis/server.js
+++ day3-restApis/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 
 const app = express();  //creating server instance
+app.use(express.json())
+
+let notes = [];
 
 
+
+app.post('/notes', (req, res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+    res.send("notes created!!!");
+})
+
+app.get("/notes", (req, res)=>{
+    console.log(notes);
+    res.send(notes)
+})
 
 
 app.listen(3000, ()=>{
