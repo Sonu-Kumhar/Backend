f0473c9f2e2688ce8989c258b7179c76cf9e7545
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-25 12:39:10 +0530

    learning: with 204 no response body is allowed while deleting a resource, and for patch we can use 200 to send success

--- day5-http-status-codes/src/app.js
+++ day5-http-status-codes/src/app.js
@@ -27,5 +27,29 @@ app.post('/notes', (req, res)=>{
     })
 })
 
+app.delete('/notes/:index', (req, res)=>{
+    delete notes[req.params.index];
+    res.status(204).json({  //
+        success:true, 
+        message:"Note deleted successfully"
+    })
+
+//     HTTP 204 ka matlab hota hai: NO CONTENT
+
+// 👉 Is status code ke saath:
+
+// Response body allowed hi nahi hota
+
+// Browser / client body ignore kar deta hai
+
+})
+
+app.patch("/notes/:index", (req, res)=>{
+    notes[req.params.index].description = req.body.description;
+
+    res.status(200).json({
+        message:"description updated successfully"
+    })
+})
 
 module.exports = app;
