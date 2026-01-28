6830d231f88af7851e770baa7224bb8afcad86ac
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-28 11:31:48 +0530

    connected the server with MongoDB locally through the mongoose package

--- day6-database/server.js
+++ day6-database/server.js
@@ -4,7 +4,16 @@
  */
 
 const app = require("./src/app")
+const mongoose = require('mongoose')
 
+function connectionToDB(){
+    mongoose.connect("mongodb://localhost:27017/day-6")
+    .then(()=>{
+        console.log("Connected with MongoDB✅")
+    })
+}
+
+connectionToDB()
 
 app.listen(3000, ()=>{
     console.log("server running on port number 3000")
