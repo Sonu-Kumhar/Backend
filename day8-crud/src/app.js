af08f11ede7c461bb59bfbda4ec1bee02028f112
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-01 11:52:36 +0530

    created post route for creating the notes

--- day8-crud/src/app.js
+++ day8-crud/src/app.js
@@ -3,14 +3,34 @@
  */
 
 const express = require("express")
+const noteModel = require("./models/note.model")
+
 
 const app = express();
-
+app.use(express.json())
 
 app.get("/", (req,res)=>{
     res.send("server is ready for crud operations")
 })
 
+/**
+ * POST /api/notes
+ * to create a new note with title and description
+ */
 
+app.post("/api/notes", async (req, res)=>{
+    console.log(req.body);
+    const {title, description} = req.body;
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })
+
+})
 
 module.exports = app;
