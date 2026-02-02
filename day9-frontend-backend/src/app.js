5d33ebe0c2c82cb07d649916de3ff5685b8a4130
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-02 12:07:03 +0530

    backend is ready to send the notes data to frontend

--- day9-frontend-backend/src/app.js
+++ day9-frontend-backend/src/app.js
@@ -1,10 +1,32 @@
 const express = require("express")
 
 const app = express();
-
+app.use(express.json())
+const noteModel = require("./models/note.model")
 
 app.get("/",(req, res)=>{
     res.send("Integrating backend with frontend")
+})
+
+app.post("/api/notes",async (req, res)=>{
+    const {title, description} = req.body;
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message:"Note created✅",
+        note
+    })
+})
+
+app.get("/api/notes", async (req, res)=>{
+    const notes = await noteModel.find();
+    res.status(200).json({
+        message:"Fetched all notes",
+        notes
+    })
 })
 
 
