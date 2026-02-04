e380b58bb8edecaa17438fe11483d3d45819a14c
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-04 11:50:08 +0530

    using axios for getting the data from backend

--- day9-frontend-backend/Frontend/src/App.jsx
+++ day9-frontend-backend/Frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios"
 
 const App = () => {
-
-  const notesData = [
+  const notesData1 = [
     {
       title: "Introduction",
       description: "This section gives an overview of the topic."
@@ -20,6 +21,19 @@ const App = () => {
       description: "Quick revision of all important points."
     }
   ];
+  const [notesData, setnotesData] = useState(notesData1)
+
+  useEffect(() => {
+
+    axios.get("http://localhost:3000/api/notes")
+      .then(res => {
+        // console.log(res.data.notes)
+        setnotesData(res.data.notes)
+      })
+
+  }, [])
+
+
 
   return (
     <div className='all-notes'>
