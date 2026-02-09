02a203aff4972cfb81d24c90a7a9b340f96839dc
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-09 18:41:35 +0530

    feat: add patch method for updating the description through react

--- day9-frontend-backend/Frontend/src/App.jsx
+++ day9-frontend-backend/Frontend/src/App.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react';
 import axios from "axios"
 
 const App = () => {
-
+  const [isEdit, setIsEdit] = useState(false)
+  const [editId, setEditId] = useState(null)
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
 
@@ -42,27 +43,54 @@ const App = () => {
 
   function submitHandler(e) {
     e.preventDefault();
-    console.log(title, description)
-    axios.post("http://localhost:3000/api/notes", {
-      title, description
-    })
-    .then((res)=>{
-      console.log(res.data)
-      getAllData();
-    })
+    // console.log("add clicked")
+    // console.log(title, description)
+
+    if (editId === null) {
+      axios.post("http://localhost:3000/api/notes", {
+        title, description
+      })
+        .then((res) => {
+          console.log(res.data)
+          getAllData();
+        })
+    }
+    else if (editId !== null) {
+      axios.patch(`http://localhost:3000/api/notes/${editId}`, {
+        description
+      })
+        .then((res) => {
+          console.log(res.data)
+          getAllData();
+          setEditId(null)
+          setIsEdit(false)
+        })
+    }
 
     setTitle("")
     setDescription("")
   }
 
-  function deleteNote(id){
+  function deleteNote(id) {
     console.log("note deleted with id", id)
 
     axios.delete(`http://localhost:3000/api/notes/${id}`)
-    .then((res)=>{
-      console.log(res.data);
-      getAllData();
-    })
+      .then((res) => {
+        console.log(res.data);
+        getAllData();
+      })
+  }
+
+  function updateNote(e, id) {
+    e.preventDefault();
+    // console.log("note updated with id ", id)
+
+    console.log("updated clicked!!!")
+    let currentNote = notesData.filter(note => note._id === id)
+
+    console.log(currentNote)
+    setTitle(currentNote[0].title)
+    setDescription(currentNote[0].description)
   }
 
 
@@ -94,7 +122,7 @@ const App = () => {
         />
 
 
-        <button>Add Note</button>
+        <button>{isEdit ? "Update Note" : "Add Note"}</button>
 
       </form>
 
@@ -104,11 +132,23 @@ const App = () => {
             return <div key={index} className='note'>
               <h2>{note.title}</h2>
               <p>{note.description}</p>
+
               <button
-                onClick={()=>{deleteNote(note._id)}}
-               className='delete'
-               >Delete
-               </button>
+                onClick={() => { deleteNote(note._id) }}
+                className='delete'
+              >Delete
+              </button>
+
+              <button
+                onClick={(e) => {
+                  updateNote(e, note._id)
+                  setIsEdit(true)
+                  setEditId(note._id)
+                }}
+                className='edit'
+              >
+                Edit
+              </button>
             </div>
           })
         }
