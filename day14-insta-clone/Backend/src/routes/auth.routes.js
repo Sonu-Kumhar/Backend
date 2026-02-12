a37da62d5075ee302219bba0c811e81b6226cb15
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-12 18:05:44 +0530

    add password hashing using bcrypt

--- day14-insta-clone/Backend/src/routes/auth.routes.js
+++ day14-insta-clone/Backend/src/routes/auth.routes.js
@@ -1,7 +1,5 @@
 const express = require("express")
-const userModel = require("../models/user.model")
-const crypto = require("crypto")
-const jwt = require("jsonwebtoken")
+const authController = require("../controllers/auth.controller")
 
 const authRouter = express.Router();
 
@@ -9,106 +7,12 @@ const authRouter = express.Router();
  * POST /api/auth/register
  */
 
-authRouter.post("/register", async (req,res)=>{
-    const {username, email, password, bio, profile_image} = req.body;
-
-    const isUserAlreadyExists = await userModel.findOne({
-        $or:[
-            { username },
-            { email }
-        ]
-    })
-
-    if(isUserAlreadyExists){
-        return res.status(409).json({
-            message:"user not exists" + (isUserAlreadyExists.email === email ? "Email already exists" : "username already exists")
-        })
-    }
-
-    const hash = crypto.createHash("sha256").update(password).digest('hex')
-
-    const user = await userModel.create({
-        username, 
-        email,
-        password : hash,
-        bio,
-        profile_image
-    })
-
-    const token = jwt.sign(
-        {
-            id:user._id
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:"1d"}
-    )
-
-    res.cookie("token", token)
-
-    res.status(201).json({
-        message : "user registered successfully",
-        user:{
-            email:user.email,
-            username: user.username,
-            bio: user.bio,
-            profile_image: user.profile_image
-        }
-    })
-})
+authRouter.post("/register", authController.registerController)
 
 /**
  * POST  /api/auth/login
  */
 
-authRouter.post("/login", async (req, res)=>{
-    const {username, email, password} = req.body;
-
-    const user = await userModel.findOne({
-        $or:[
-            {
-                username:username
-            },
-            {
-                email:email
-            }
-        ]
-    })
-
-    if(!user){
-        return res.status(404).json({
-            message:"user not exists"
-        })
-    }
-
-    const hash = crypto.createHash("sha256").update(password).digest("hex");
-
-    const isPasswordValid = hash === user.password;
-
-    if(!isPasswordValid){
-        return res.status(401).json({
-            message:"password is incorrect"
-        })
-    }
-
-    const token = jwt.sign(
-        {
-            id : user._id
-        },
-        process.env.JWT_SECRET,
-        {expiresIn:"1d"}
-    )
-
-    res.cookie("token", token);
-
-    res.status(200).json({
-        message:"user loggedIn successfully",
-        user:{
-            username:user.username,
-            email:user.email,
-            bio:user.bio,
-            profile_image:user.profile_image
-        }
-    })
-})
+authRouter.post("/login", authController.loginController)
 
 module.exports = authRouter
