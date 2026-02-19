3cf518124d597c94b95ba86ec712371ed147516d
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-19 17:29:00 +0530

    feat: add login feature

--- day14-insta-clone/Frontend/src/features/auth/pages/Login.jsx
+++ day14-insta-clone/Frontend/src/features/auth/pages/Login.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router'
+import axios from "axios"
+import { useState } from 'react'
 
 const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function handleSubmit(e){
+        e.preventDefault()
+
+        axios.post("http://localhost:3000/api/auth/login", {
+            username,
+            password
+        },{
+            withCredentials: true
+        })
+        .then(res => {
+            console.log(res.data)
+        })
+    }
+
   return (
         <main>
             <div className='form-container'>
                 <h1>Login</h1>
 
-                <form>
-                    <input type="text"  placeholder='Enter username'/>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    onInput={(e)=>{setUsername(e.target.value)}}
+                    type="text"  
+                    name='username'
+                    placeholder='Enter username'
+                    />
+                    <input type='password' 
+                    onInput={(e)=>{setPassword(e.target.value)}}
+                    name='password'
+                    placeholder='enter password'
+                    />
+                    <button>Login</button>
                 </form>
+
+                <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Register</Link></p>
             </div>
         </main>
   )
