4a0102b89c3eb5a7fb2c32dbbe7887a2d01ff518
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-21 10:41:00 +0530

    refactor: use 4 layer react architecture

--- day14-insta-clone/Backend/src/app.js
+++ day14-insta-clone/Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 
 const cookieParser = require("cookie-parser")
 
@@ -9,6 +10,10 @@ const userRouter = require("./routes/user.routes")
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
 
 app.use("/api/auth", authRouter)
 app.use("/api/posts", postRouter)
