8aa32799f411b372a1d381c082ca63f070380390
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-21 11:17:37 +0530

    feat: add morgan package to see the request results

--- day14-insta-clone/Backend/src/app.js
+++ day14-insta-clone/Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const morgan = require("morgan")
 
 const cookieParser = require("cookie-parser")
 
@@ -10,6 +11,8 @@ const userRouter = require("./routes/user.routes")
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(morgan())
+
 app.use(cors({
     credentials: true,
     origin: "http://localhost:5173"
