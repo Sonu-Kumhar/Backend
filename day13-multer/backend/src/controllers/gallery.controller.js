fc9238d87f9a4773e7069bddfa32840b5392eef7
Sonu Kumhar <kumharsonu899@gmail.com>
2026-02-24 11:30:41 +0530

    feat: add controllers in gallery routes

---
+++ day13-multer/backend/src/controllers/gallery.controller.js
@@ -0,0 +1,34 @@
+const ImageKit = require('@imagekit/nodejs');
+const { toFile } = require('@imagekit/nodejs');
+const galleryModel = require("../models/gallery")
+
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+})
+
+async function createGalleryController (req, res) {
+    // console.log(req.body, req.file)
+
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: 'avatar',
+        folder: "multer"
+    });
+
+    console.log(file)
+
+    const photo = await galleryModel.create({
+        title: req.body.title,
+        description: req.body.description,
+        imgUrl: file.url
+    })
+
+    res.status(201).json({
+        message: "photo uploaded successfully on your gallery",
+        photo
+    })
+}
+
+module.exports = {
+    createGalleryController
+}
--- day13-multer/backend/src/routes/gallery.routes.js
+++ day13-multer/backend/src/routes/gallery.routes.js
@@ -3,41 +3,15 @@ const galleryRouter = express.Router();
 const multer = require("multer")
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
-const ImageKit = require('@imagekit/nodejs');
-const { toFile } = require('@imagekit/nodejs');
-const galleryModel = require("../models/gallery")
+const galleryController = require('../controllers/gallery.controller')
 
-const imagekit = new ImageKit({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
-})
 
 /**
  * @route POST /api/gallery
  * @description upload a file from the frontend
  */
 
-galleryRouter.post("/", upload.single("avatar"), async (req, res) => {
-    // console.log(req.body, req.file)
-
-    const file = await imagekit.files.upload({
-        file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: 'avatar',
-        folder: "multer"
-    });
-
-    console.log(file)
-
-    const photo = await galleryModel.create({
-        title: req.body.title,
-        description: req.body.description,
-        imgUrl: file.url
-    })
-
-    res.status(201).json({
-        message: "photo uploaded successfully on your gallery",
-        photo
-    })
-})
+galleryRouter.post("/", upload.single("avatar"), galleryController.createGalleryController)
 
 
 
